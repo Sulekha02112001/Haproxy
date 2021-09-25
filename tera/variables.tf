b2b5d1210e0d9e671edc1e361e7ae24be6e4e558
Sulekha02112001 <64317049+Sulekha02112001@users.noreply.github.com>
2021-09-25 13:07:21 +0530

    Update variables.tf

--- tera/variables.tf
+++ tera/variables.tf
@@ -1,29 +1,29 @@
 variable "docker_flask_folder_path" {
   type        = string
-  description = "The id of the machine image (AMI) to use for the server."
+  description = "Path of folder."
   default     = "./flaskApp/"
 }
 variable "image_name_flask" {
   type        = string
-  description = "The id of the machine image (AMI) to use for the server."
+  description = "image name for flask app"
   default     = "flask"
 }
 
 variable "docker_loadbalancer_folder_path" {
-  type        = list(any)
-  description = "The id of the machine image (AMI) to use for the server."
+  type        = list
+  description = "path of haproxy folder"
   default     = ["./haproxy1/", "./haproxy2"]
 }
 
 variable "image_name" {
-  type        = list(any)
-  description = "The id of the machine image (AMI) to use for the server."
+  type        = list
+  description = "image name of haproxy"
   default     = ["haproxy1", "haproxy2"]
 }
 
 variable "name" {
-  type        = list(any)
-  description = "value"
+  type        = list
+  description = "container name of haproxy"
   default     = ["haproxy-1", "haproxy-2"]
 
 }
